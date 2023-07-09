2c6dac31920337bc0c12acd1d784cee33dd3fec3
unknown <lnikiema9@gmail.com>
2023-07-09 16:29:21 +0000

    creation des templates

--- webmapping/urls.py
+++ webmapping/urls.py
@@ -2,5 +2,6 @@ from  django.urls import  path
 from  . import views
 
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('', views.home, name='home'),
+    path('login', views.login, name='login'),
 ]
