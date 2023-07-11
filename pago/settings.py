842c14f7fa83d78d109db779b08d28276befa28d
unknown <lnikiema9@gmail.com>
2023-07-11 16:35:56 +0000

    django jazzmin add to customize site adminsitration

--- pago/settings.py
+++ pago/settings.py
@@ -188,7 +188,7 @@ JAZZMIN_SETTINGS = {
     "welcome_sign": "Bienvenue administration des données du web mappin",
 
     # Copyright on the footer
-    "copyright": "logo-services 2023",
+    "copyright": "logo-services",
 
     # List of model admins to search from the search bar, search bar omitted if excluded
     # If you want to use a single search field you dont need to use a list, you can use a simple string 
@@ -247,14 +247,14 @@ JAZZMIN_SETTINGS = {
     "order_with_respect_to": ["auth", "webmapping"],
 
     # Custom links to append to app groups, keyed on app name
-    "custom_links": {
-        "webmapping": [{
-            "name": "Make Messages", 
-            "url": "make_messages", 
-            "icon": "fas fa-comments",
-            "permissions": ["books.view_book"]
-        }]
-    },
+   # "custom_links": {
+    #    "webmapping": [{
+            #"name": "Make Messages", 
+           # "url": "make_messages", 
+    #        "icon": "fas fa-comments",
+    #        "permissions": ["books.view_book"]
+    #    }]
+    #},
 
     # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
     # for the full list of 5.13.0 free icon classes
@@ -298,4 +298,12 @@ JAZZMIN_SETTINGS = {
     "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
     # Add a language dropdown into the admin
     #"language_chooser": True,
+}
+
+
+JAZZMIN_UI_TWEAKS = {
+    "theme": "slate",
+    "accent": "accent-primary",
+    "navbar": "navbar-white navbar-light",
+  
 }
