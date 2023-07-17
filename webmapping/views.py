c5e3842615ff2ecce170e32140e5a5cda796c83f
unknown <lnikiema9@gmail.com>
2023-07-17 18:01:47 +0000

    gis data from database

--- webmapping/views.py
+++ webmapping/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from webmapping.models import *
+import json
 
 
 
 
 
 def map(request):
-    return render(request, 'webmapping/map.html')
+    results = Infrastructure.objects.all() 
+    results_json = json.dumps(list(results.values()))
+    context = {'results_json': results_json}
+    return render(request, 'webmapping/map.html', context)
+
+
+
 
 
 
