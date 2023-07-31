ec8b321c50bb05082e79464627164f90e59e830e
sougrinoma-sawadogo <reckah01@gmail.com>
2023-07-31 20:08:26 +0000

    Move /clone to /

--- webmapping/urls.py
+++ webmapping/urls.py
@@ -2,7 +2,7 @@ from  django.urls import  path
 from  . import views
 
 urlpatterns = [
-    path('',  views.map, name='map'),
+    path('',  views.clone, name='clone'),
     path('clone/',  views.clone, name='clone'),
     path('api/types/', views.get_types, name='get_types'),
     path('api/locations/', views.get_locations, name='get_locations'),
