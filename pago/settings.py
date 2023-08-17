f3f4fa4fa8948e90dcaeb36512c097c2acc3ab09
laurenzo1992 <lnikiema9@gmail.com>
2023-08-17 17:25:54 +0000

    un test

--- pago/settings.py
+++ pago/settings.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = "pago.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -81,14 +81,17 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
-""" DATABASES = {
-    "default": {
-        "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "NAME": "pago",
-        "USER": "postgres",
-    },
-} """
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pago',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 """
